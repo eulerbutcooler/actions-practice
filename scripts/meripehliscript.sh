8a4c1d1a8c7f77bc4419afd1a8ef6ecd0c0ff20e
eulerbutcooler <eulerbutcooler@gmail.com>
2026-02-18 04:39:14 +0530

    Trying out scripts

---
+++ scripts/meripehliscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script will print "Hello, World!" to the console
+
+echo "Hello, World!"
+if [ $? -eq 0];
+then
+    echo "Script executed successfully!"
+else
+    echo "Script execution failed!"
+fi
